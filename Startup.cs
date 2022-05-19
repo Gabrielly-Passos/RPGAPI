aabe61e109a8de85ad719661c9be78f8ff97639d
Gabrielly Passos paulista <passosgabrielly03@gmail.com>
2022-05-19 18:06:58 -0300

    [UPDATE] Projeto Desenvolvimento 19.05.2022

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace RpgApi
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("ConexaoLocal")));  
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("ConexaoFreeAsHosting")));  
 
             services.AddControllers();
 
